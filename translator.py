d76c11613f0ac83b457ee027f6adb22ea91d32a2
Alex Yang <alex.yang@berkeley.edu>
2022-01-08 15:31:12 -0800

    added some linking verbs

--- translator.py
+++ translator.py
@@ -38,8 +38,10 @@ def get_translation(text):
     # 2. Search for noun chunks and for later to modify words that are root nouns
     doc = nlp(replaced)
     valid_nouns = set()
+    valid_verbs = {token.text for token in doc if token.tag_ in {"VB", "VBP", "VBZ"}}
     for chunk in doc.noun_chunks:
-        valid_nouns.add(chunk.root.text)
+        if chunk.root.tag_ == "NN":
+            valid_nouns.add(chunk.root.text)
 
     # 3. if there ever exists 's a ', combine into 'sa '
     replaced = replaced.replace("s a ", "sa ")
@@ -54,7 +56,6 @@ def get_translation(text):
         else:
             if curr_word.lower() in key_words:
                 translated_word = key_words[curr_word.lower()]
-                # we keep note of what capitalization the user may have had
                 if curr_word == curr_word.upper() and curr_word != "I":
                     res += translated_word.upper()
                 elif curr_word == curr_word.capitalize():
@@ -67,14 +68,14 @@ def get_translation(text):
             else:
                 # 4. use othe rules to spice up the rest
                 if curr_word:
-                    res += spice_up(curr_word, valid_nouns)
+                    res += spice_up(curr_word, valid_nouns, valid_verbs)
             if i != len(replaced)-1:
                 res += ch
             curr_word = ""
 
     return res
 
-def spice_up(word, valid_nouns):
+def spice_up(word, valid_nouns, valid_verbs):
     """
     Add some gungan type slang to the word.
     """
@@ -87,24 +88,20 @@ def spice_up(word, valid_nouns):
             word = word[:-3] + "en"
 
     elif word[-1].lower() == "e":
-        doc = nlp(word)
-        word_type = doc[0].tag_
-        if word_type in {"VB", "VBP", "VBZ"}:
+        if word in valid_verbs:
             if all_caps:
                 word += "N"
             else:
                 word += "n"
-        elif word_type == "NN" and len(word) > 1 and word[-2].lower() != "s" and word in valid_nouns: # may remove
+        elif len(word) > 1 and word[-2].lower() not in "scxqzj" and word in valid_nouns: # may remove
             if all_caps:
                 word += "SA"
             else:
                 word += "sa"
 
     elif word[-1].upper() in good_consonants:
-        if len(word) > 1 and word[-2].lower() not in {"a", "e", "i", "o", "u", "y"}:
-            doc = nlp(word)
-            word_type = doc[0].tag_
-            if word_type in {"VB", "VBP", "VBZ"} or (word_type == "NN" and word in valid_nouns):
+        if len(word) > 1 and word[-2].lower() not in "aeiouy":
+            if word in valid_verbs or word in valid_nouns:
                 if all_caps:
                     word += "EN"
                 else:
